0813a5ac4b0f919871b35fded70fff56fe36b400
Jack Zampolin <jack.zampolin@gmail.com>
2021-05-19 09:28:32 -0700

    WIP commands

--- module/x/gravity/client/cli/tx.go
+++ module/x/gravity/client/cli/tx.go
@@ -1,7 +1,13 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/gravity-bridge/module/x/gravity/types"
@@ -17,30 +23,61 @@ func GetTxCmd(storeKey string) *cobra.Command {
 	}
 
 	gravityTxCmd.AddCommand([]*cobra.Command{
-		// CmdSendToEth(),
-		// CmdRequestBatch(),
-		// CmdSetOrchestratorAddress(),
-		// GetUnsafeTestingCmd(),
+		CmdSendToEthereum(),
+		// CmdCancelSendToEthereum(),
+		// CmdRequestBatchTx(),
+		// CmdSubmitEthereumSignature(),
+		// CmdSubmitEthereumEvent(),
+		// CmdSetDelegateKeys(),
 	}...)
 
 	return gravityTxCmd
 }
+func CmdSendToEthereum() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "send-to-etheruem [ethereum-reciever] [send-coins] [fee-coins]",
+		Aliases: []string{"send", "transfer"},
+		Args:    cobra.ExactArgs(3),
+		Short:   "Send tokens from cosmos chain to connected etheruem chain",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
 
-// func GetUnsafeTestingCmd() *cobra.Command {
-// 	testingTxCmd := &cobra.Command{
-// 		Use:                        "unsafe_testing",
-// 		Short:                      "helpers for testing. not going into production",
-// 		DisableFlagParsing:         true,
-// 		SuggestionsMinimumDistance: 2,
-// 		RunE:                       client.ValidateCmd,
-// 	}
-// 	testingTxCmd.AddCommand([]*cobra.Command{
-// 		CmdUnsafeETHPrivKey(),
-// 		CmdUnsafeETHAddr(),
-// 	}...)
+			from := clientCtx.GetFromAddress()
+			if from == nil {
+				return fmt.Errorf("must pass from flag")
+			}
 
-// 	return testingTxCmd
-// }
+			if !common.IsHexAddress(args[0]) {
+				return fmt.Errorf("must be a valid etheruem address got %s", args[0])
+			}
+
+			// Get amount of coins
+			sendCoin, err := sdk.ParseCoinNormalized(args[1])
+			if err != nil {
+				return err
+			}
+
+			feeCoin, err := sdk.ParseCoinNormalized(args[2])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgSendToEthereum(from, common.HexToAddress(args[0]).Hex(), sendCoin, feeCoin)
+			if err = msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
 
 // func CmdUnsafeETHPrivKey() *cobra.Command {
 // 	return &cobra.Command{
@@ -80,101 +117,4 @@ func GetTxCmd(storeKey string) *cobra.Command {
 // 			return nil
 // 		},
 // 	}
-// }
-
-// func CmdSendToEth() *cobra.Command {
-// 	cmd := &cobra.Command{
-// 		Use:   "send-to-eth [eth-dest] [amount] [bridge-fee]",
-// 		Short: "Adds a new entry to the transaction pool to withdraw an amount from the Ethereum bridge contract",
-// 		Args:  cobra.ExactArgs(3),
-// 		RunE: func(cmd *cobra.Command, args []string) error {
-// 			cliCtx, err := client.GetClientTxContext(cmd)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			cosmosAddr := cliCtx.GetFromAddress()
-
-// 			amount, err := sdk.ParseCoinsNormalized(args[1])
-// 			if err != nil {
-// 				return sdkerrors.Wrap(err, "amount")
-// 			}
-// 			bridgeFee, err := sdk.ParseCoinsNormalized(args[2])
-// 			if err != nil {
-// 				return sdkerrors.Wrap(err, "bridge fee")
-// 			}
-
-// 			if len(amount) > 1 || len(bridgeFee) > 1 {
-// 				return fmt.Errorf("coin amounts too long, expecting just 1 coin amount for both amount and bridgeFee")
-// 			}
-
-// 			// Make the message
-// 			msg := types.MsgSendToEthereum{
-// 				Sender:    cosmosAddr.String(),
-// 				EthDest:   args[0],
-// 				Amount:    amount[0],
-// 				BridgeFee: bridgeFee[0],
-// 			}
-// 			if err := msg.ValidateBasic(); err != nil {
-// 				return err
-// 			}
-// 			// Send it
-// 			return tx.GenerateOrBroadcastTxCLI(cliCtx, cmd.Flags(), &msg)
-// 		},
-// 	}
-// 	flags.AddTxFlagsToCmd(cmd)
-// 	return cmd
-// }
-
-// func CmdRequestBatch() *cobra.Command {
-// 	cmd := &cobra.Command{
-// 		Use:   "build-batch [token_contract_address]",
-// 		Short: "Build a new batch on the cosmos side for pooled withdrawal transactions",
-// 		Args:  cobra.ExactArgs(1),
-// 		RunE: func(cmd *cobra.Command, args []string) error {
-// 			cliCtx, err := client.GetClientTxContext(cmd)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			cosmosAddr := cliCtx.GetFromAddress()
-
-// 			// TODO: better denom searching
-// 			msg := types.MsgRequestBatchTx{
-// 				Sender: cosmosAddr.String(),
-// 				Denom:  fmt.Sprintf("gravity%s", args[0]),
-// 			}
-// 			if err := msg.ValidateBasic(); err != nil {
-// 				return err
-// 			}
-// 			// Send it
-// 			return tx.GenerateOrBroadcastTxCLI(cliCtx, cmd.Flags(), &msg)
-// 		},
-// 	}
-// 	flags.AddTxFlagsToCmd(cmd)
-// 	return cmd
-// }
-
-// func CmdSetOrchestratorAddress() *cobra.Command {
-// 	cmd := &cobra.Command{
-// 		Use:   "set-orchestrator-address [validator-address] [orchestrator-address] [ethereum-address]",
-// 		Short: "Allows validators to delegate their voting responsibilities to a given key.",
-// 		Args:  cobra.ExactArgs(3),
-// 		RunE: func(cmd *cobra.Command, args []string) error {
-// 			cliCtx, err := client.GetClientTxContext(cmd)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			msg := types.MsgSetOrchestratorAddress{
-// 				Validator:    args[0],
-// 				Orchestrator: args[1],
-// 				EthAddress:   args[2],
-// 			}
-// 			if err := msg.ValidateBasic(); err != nil {
-// 				return err
-// 			}
-// 			// Send it
-// 			return tx.GenerateOrBroadcastTxCLI(cliCtx, cmd.Flags(), &msg)
-// 		},
-// 	}
-// 	flags.AddTxFlagsToCmd(cmd)
-// 	return cmd
 // }
