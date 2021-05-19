fedc2f1dab5abaf1a3c89a65fd080c60a5c89945
Levi Cook <levicook@gmail.com>
2021-05-19 12:21:45 -0600

    stubbing out cli - gravity query batch-tx and contract-call-tx stubs

--- module/x/gravity/client/cli/query.go
+++ module/x/gravity/client/cli/query.go
@@ -1,8 +1,6 @@
 package cli
 
 import (
-	"context"
-
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/gravity-bridge/module/x/gravity/types"
@@ -20,8 +18,8 @@ func GetQueryCmd() *cobra.Command {
 	gravityQueryCmd.AddCommand(
 		CmdParams(),
 		CmdSignerSetTx(),
-		// CmdBatchTx(),
-		// CmdContractCallTx(),
+		CmdBatchTx(),
+		CmdContractCallTx(),
 		// CmdSignerSetTxs(),
 		// CmdBatchTxs(),
 		// CmdContractCallTxs(),
@@ -94,6 +92,78 @@ func CmdSignerSetTx() *cobra.Command {
 			}
 
 			res, err := queryClient.SignerSetTx(cmd.Context(), &req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func CmdBatchTx() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "batch-tx [contract-address] [nonce]",
+		Args:  cobra.ExactArgs(2),
+		Short: "", // TODO(levi) provide short description
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			var ( // args
+				contractAddress string // TODO(levi) init and validate from args[0]
+				nonce           uint64 // TODO(levi) init and validate from args[1]
+			)
+
+			req := types.BatchTxRequest{
+				ContractAddress: contractAddress,
+				Nonce:           nonce,
+			}
+
+			res, err := queryClient.BatchTx(cmd.Context(), &req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func CmdContractCallTx() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "contract-call-tx [invalidation-scope] [invalidation-nonce]",
+		Args:  cobra.ExactArgs(2),
+		Short: "", // TODO(levi) provide short description
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			var ( // args
+				invalidationScope []byte // TODO(levi) init and validate from args[0]
+				invalidationNonce uint64 // TODO(levi) init and validate from args[1]
+			)
+
+			req := types.ContractCallTxRequest{
+				InvalidationScope: invalidationScope,
+				InvalidationNonce: invalidationNonce,
+			}
+
+			res, err := queryClient.ContractCallTx(cmd.Context(), &req)
 			if err != nil {
 				return err
 			}
