4340ff0fa67bf8276fd7773a8feedca4d5b6d1de
Levi Cook <levicook@gmail.com>
2021-05-19 14:52:02 -0600

    stubbing out cli query stub: batch-tx-fees

--- module/x/gravity/client/cli/query.go
+++ module/x/gravity/client/cli/query.go
@@ -30,7 +30,7 @@ func GetQueryCmd() *cobra.Command {
 		CmdPendingBatchTxEthereumSignatures(),
 		CmdPendingContractCallTxEthereumSignatures(),
 		CmdLastSubmittedEthereumEvent(),
-		// CmdBatchTxFees(),
+		CmdBatchTxFees(),
 		// CmdERC20ToDenom(),
 		// CmdDenomToERC20(),
 		// CmdPendingSendToEthereums(),
@@ -502,6 +502,34 @@ func CmdLastSubmittedEthereumEvent() *cobra.Command {
 			}
 
 			res, err := queryClient.LastSubmittedEthereumEvent(cmd.Context(), &req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func CmdBatchTxFees() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "batch-tx-fees",
+		Args:  cobra.NoArgs,
+		Short: "", // TODO(levi) provide short description
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			req := types.BatchTxFeesRequest{}
+
+			res, err := queryClient.BatchTxFees(cmd.Context(), &req)
 			if err != nil {
 				return err
 			}
