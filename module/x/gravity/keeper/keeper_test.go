9a8a0a0b261d0f7b750969e1754f9bd8923a56e7
Jtremback <hi@jehan.email>
2021-05-19 15:56:56 -0700

    get tests passing after deleting specialized outgoinTx last slashed code

--- module/x/gravity/keeper/keeper_test.go
+++ module/x/gravity/keeper/keeper_test.go
@@ -257,29 +257,29 @@ func TestLastSlashedValsetNonce(t *testing.T) {
 	assert.Equal(t, uint64(i-1), latestValsetNonce)
 
 	//  lastSlashedValsetNonce should be zero initially.
-	lastSlashedValsetNonce := k.GetLastSlashedSignerSetTxNonce(ctx)
+	lastSlashedValsetNonce := k.GetLastSlashedOutgoingTxBlockHeight(ctx)
 	assert.Equal(t, uint64(0), lastSlashedValsetNonce)
-	unslashedValsets := k.GetUnSlashedSignerSetTxs(ctx, uint64(12))
+	unslashedValsets := k.GetUnSlashedOutgoingTxs(ctx, uint64(12))
 	assert.Equal(t, 9, len(unslashedValsets))
 
 	// check if last Slashed Valset nonce is set properly or not
-	k.SetLastSlashedSignerSetTxNonce(ctx, uint64(3))
-	lastSlashedValsetNonce = k.GetLastSlashedSignerSetTxNonce(ctx)
+	k.SetLastSlashedOutgoingTxBlockHeight(ctx, uint64(3))
+	lastSlashedValsetNonce = k.GetLastSlashedOutgoingTxBlockHeight(ctx)
 	assert.Equal(t, uint64(3), lastSlashedValsetNonce)
 
 	// when maxHeight < lastSlashedValsetNonce, len(unslashedValsets) should be zero
-	unslashedValsets = k.GetUnSlashedSignerSetTxs(ctx, uint64(2))
+	unslashedValsets = k.GetUnSlashedOutgoingTxs(ctx, uint64(2))
 	assert.Equal(t, 0, len(unslashedValsets))
 
 	// when maxHeight == lastSlashedValsetNonce, len(unslashedValsets) should be zero
-	unslashedValsets = k.GetUnSlashedSignerSetTxs(ctx, uint64(3))
+	unslashedValsets = k.GetUnSlashedOutgoingTxs(ctx, uint64(3))
 	assert.Equal(t, 0, len(unslashedValsets))
 
 	// when maxHeight > lastSlashedValsetNonce && maxHeight <= latestValsetNonce
-	unslashedValsets = k.GetUnSlashedSignerSetTxs(ctx, uint64(6))
+	unslashedValsets = k.GetUnSlashedOutgoingTxs(ctx, uint64(6))
 	assert.Equal(t, 2, len(unslashedValsets))
 
 	// when maxHeight > latestValsetNonce
-	unslashedValsets = k.GetUnSlashedSignerSetTxs(ctx, uint64(15))
+	unslashedValsets = k.GetUnSlashedOutgoingTxs(ctx, uint64(15))
 	assert.Equal(t, 6, len(unslashedValsets))
 }
