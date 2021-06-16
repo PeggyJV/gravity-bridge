cf8a00fde7bbcb64da62ce0801e49574d284b5c6
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-06-16 12:44:51 -0700

    unused code

--- module/cmd/gravity/cmd/root_test.go
+++ module/cmd/gravity/cmd/root_test.go
@@ -2,43 +2,15 @@ package cmd
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/cli"
-	"io"
-	"os"
 	"strings"
 	"testing"
 )
-
-
-func executeCommandC(root *cobra.Command, args ...string) (c *cobra.Command, output string, err error) {
-	buf := new(bytes.Buffer)
-	root.SetOut(buf)
-	root.SetErr(buf)
-	root.SetArgs(args)
-
-	c, err = root.ExecuteC()
-
-	return c, buf.String(), err
-}
-
-
-func executeCommandWithContext(ctx context.Context, root *cobra.Command, args ...string) (output string, err error) {
-	buf := new(bytes.Buffer)
-	root.SetOut(buf)
-	root.SetErr(buf)
-	root.SetArgs(args)
-
-	err = root.ExecuteContext(ctx)
-
-	return buf.String(), err
-}
 
 type KeyOutput struct {
 	Name string `json:"name"`
@@ -47,25 +19,7 @@ type KeyOutput struct {
 	PubKey string `json:"pubkey"`
 }
 
-func captureStdout(f func()) string {
-	old := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	f()
-
-	w.Close()
-	os.Stdout = old
-
-	var buf bytes.Buffer
-	io.Copy(&buf, r)
-	return buf.String()
-}
-
 func TestKeyGen(t *testing.T) {
-	//rootCmd, config := NewRootCmd()
-	//t.Log(config)
-
 	buf := bytes.NewBuffer(nil)
 
 	mnemonic := "weasel lunch attack blossom tone drum unfair worry risk level negative height sight nation inside task oyster client shiver aware neck mansion gun dune"
