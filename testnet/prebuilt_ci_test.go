a04cd271cca82182476be0e21866968ff1204461
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-07-08 13:58:36 -0700

    in both tests

--- testnet/prebuilt_ci_test.go
+++ testnet/prebuilt_ci_test.go
@@ -97,6 +97,40 @@ func TestPrebuiltCi(t *testing.T) {
 	appState, genDoc, err := types.GenesisStateFromGenFile(genFilePath)
 	require.NoError(t, err, "error reading genesis file")
 
+	var bank Bank
+	err = json.Unmarshal(appState["bank"], &bank)
+	require.NoError(t, err, "error unmarshalling bank genesis state")
+	bank.DenomMetadata = append(bank.DenomMetadata, DenomMetadata{
+		Description: "footoken",
+		Display:     "mfootoken",
+		Base:        "footoken",
+		DenomUnits: []DenomUnit{
+			{
+				Denom: "footoken",
+				Exponent: 0,
+			},
+			{
+				Denom: "mfootoken",
+				Exponent: 6,
+			},
+		},
+	})
+	bank.DenomMetadata = append(bank.DenomMetadata, DenomMetadata{
+		Description: "stake",
+		Display:     "mstake",
+		Base:        "stake",
+		DenomUnits: []DenomUnit{
+			{
+				Denom: "stake",
+				Exponent: 0,
+			},
+			{
+				Denom: "mstake",
+				Exponent: 3,
+			},
+		},
+	})
+
 	var genUtil GenUtil
 	err = json.Unmarshal(appState["genutil"], &genUtil)
 	require.NoError(t, err, "error unmarshalling genesis state")
