3462f90fa1ad77386fe15881be8d341811c6342e
Levi Cook <levicook@gmail.com>
2021-07-11 01:16:56 +0000

    wip: figuring out out to use the signatory pre-release

--- orchestrator/key_tools/src/lib.rs
+++ orchestrator/key_tools/src/lib.rs
@@ -20,9 +20,30 @@
 mod tests {
     use hkd32::mnemonic;
     use rand_core;
+    use signatory::ecdsa::secp256k1;
+    use signatory::FsKeyStore;
+    use signatory::GeneratePkcs8;
 
     // NOTE these are not _real_ tests. I'm using them to bring together dependencies and learn how to use them.
     // NOTE run w/ `cargo test -- --nocapture` to see the println
+
+    #[test]
+    fn load_key_by_name() {
+        let tmp = tempfile::tempdir().expect("Failed to create temp dir!");
+        let keystore = FsKeyStore::create_or_open(&tmp.path().join("key-store"))
+            .expect("Failed to create or open key-store");
+
+        let my_key = secp256k1::SigningKey::generate_pkcs8();
+
+        let key_name = "my_key".parse().expect("Failed to parse key_name");
+
+        keystore
+            .store(&key_name, &my_key)
+            .expect("Failed to store my_key!");
+
+        let key_info = keystore.info(&key_name).expect("Failed to info my_key!");
+        println!(">> {:?}", key_info);
+    }
 
     #[test]
     fn create_random_mnemonic_phrase() {
@@ -33,8 +54,8 @@ mod tests {
     #[test]
     fn recover_phrase_from_mnemonic() {
         const MNEMONIC: &str = "save able shop proud seek reflect prepare mechanic armor car core shuffle room axis file diet axis try secret evolve opinion prosper flush buyer";
-        let p = mnemonic::Phrase::new(MNEMONIC, mnemonic::Language::English);
-        let p = p.unwrap();
+        let p = mnemonic::Phrase::new(MNEMONIC, mnemonic::Language::English)
+            .expect("Failed to create phrase!");
         assert_eq!(MNEMONIC, p.phrase())
     }
 }
