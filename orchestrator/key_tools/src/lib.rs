305cfa2bae35f7f23298a6437c90c61b57a8192e
Hannydevelop <ukpaiugochiibem @gmail.com>
2021-07-13 18:23:40 +0100

    I still have some errors, fixing mnemonic_phrase_to_pkcs8

--- orchestrator/key_tools/src/lib.rs
+++ orchestrator/key_tools/src/lib.rs
@@ -40,10 +40,13 @@ mod tests {
     //   -- maybe derive_subkey?? or possibly to_seed?? (latter requires a password)
     // - What "display formats" do we care about?
     //   -- and where can I find examples??
-    use bip32::{Mnemonic, XPrv};
-    use pkcs8::ToPrivateKey;
+    use hkd32;
     use rand_core::OsRng;
-    use signatory::keystore::FsKeyStore;
+    use signatory::ecdsa::secp256k1;
+    use signatory::FsKeyStore;
+    use signatory::GeneratePkcs8;
+    use pkcs8::ToPrivateKey;
+    use bip32::{Mnemonic, XPrv};
     use std::path::Path;
 
     const RECOVERY_PHRASE: &str = "save able shop proud seek reflect prepare mechanic armor car core shuffle room axis file diet axis try secret evolve opinion prosper flush buyer";
@@ -64,14 +67,15 @@ mod tests {
 
         println!{"{}", mnemonic.phrase()};
 
-        let seed = mnemonic.to_seed("TREZOR"); // todo: password argument
-        let xprv = XPrv::new(&seed).unwrap();
+        let seed = mnemonic.to_seed("MYPASSWORD"); // todo: password argument
+        let xprv = XPrv::new(seed).unwrap();
         let private_key_der = k256::SecretKey::from(xprv.private_key()).to_pkcs8_der();
-        let keystore_path = Path::new("/tmp/keystore");
+        let keystore_path = Path::new("keystore");
         if !keystore_path.exists() {
             FsKeyStore::create(keystore_path).unwrap();
         }
         let keystore = FsKeyStore::open(keystore_path).unwrap();
+        keystore.store(&name: String, &private_key_der).unwrap();
         // key_material.as_bytes()
 
         // let _signatory_key: signatory::pkcs8::PrivateKeyDocument;
@@ -79,5 +83,71 @@ mod tests {
         // TODO(ugochi): figure out how to init _signatory_key from _phrase
     }
 
-    
+    #[test]
+    fn store_load_and_delete_my_key() {
+        let tempdir = tempfile::tempdir().expect("Could not create tempdir");
+
+        let keystore = tempdir.path().join("keystore");
+        println!(">> {:?}", keystore);
+
+        let keystore =
+            FsKeyStore::create_or_open(&keystore).expect("Could not create or open keystore");
+
+        let key_name = "my_key".parse().expect("Could not parse key name");
+
+        keystore
+            .store(&key_name, &secp256k1::SigningKey::generate_pkcs8())
+            .expect("Could not store key");
+
+        let key_info = keystore.info(&key_name).expect("Could not load key info");
+        println!(">> {:?}", key_info);
+
+        keystore.delete(&key_name).expect("Could not delete key");
+    }
+
+    #[test]
+    fn explore_hkd32_mnemonic_create_phrase() {
+        let lang = hkd32::mnemonic::Language::English;
+        let p = hkd32::mnemonic::Phrase::random(&mut rand_core::OsRng, lang);
+        println!(">> {}", p.phrase());
+    }
+
+    #[test]
+    fn explore_hkd32_mnemonic_recover_phrase() {
+        let lang = hkd32::mnemonic::Language::English;
+        let p = hkd32::mnemonic::Phrase::new(RECOVERY_PHRASE, lang);
+        let p = p.expect("Failed to create phrase!");
+        assert_eq!(p.phrase(), RECOVERY_PHRASE);
+
+        // Things you can do with p:
+        // p.derive_subkey(path: impl AsRef<Path>)
+        // p.to_seed(password: &str)
+        // p.entropy();
+        // p.language()
+    }
+
+    #[test]
+    fn explore_deep_space_from_phrase() {
+        let pass_phrase = "";
+        let _deep_space_key = deep_space::PrivateKey::from_phrase(RECOVERY_PHRASE, pass_phrase)
+            .expect("Could not generate key from phrase");
+
+        // _deep_space_key.0
+    }
+
+    #[test]
+    fn explore_xxx() {
+        // use signatory::ecdsa::elliptic_curve::AlgorithmParameters;
+        // let oid = signatory::ecdsa::Secp256k1::OID;
+
+        let oid = signatory::ecdsa::elliptic_curve::ALGORITHM_OID;
+        let oid = signatory::pkcs8::ObjectIdentifier::from(oid);
+
+        let aid = signatory::pkcs8::AlgorithmIdentifier {
+            oid,
+            parameters: None,
+        };
+
+        signatory::pkcs8::PrivateKeyInfo::new(aid, &[]);
+    }
 }
