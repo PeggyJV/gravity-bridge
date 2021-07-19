8a3fee7466e6e6ec4c89f76dd86e00893246acac
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-07-19 08:19:23 -0400

    update tx

--- module/x/gravity/client/cli/tx.go
+++ module/x/gravity/client/cli/tx.go
@@ -143,9 +143,12 @@ func CmdRequestBatchTx() *cobra.Command {
 
 func CmdSetDelegateKeys() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "set-delegate-keys [validator-address] [orchestrator-address] [ethereum-address]",
+		Use:   "set-delegate-keys [validator-address] [orchestrator-address] [ethereum-address] [ethereum-signature]",
 		Args:  cobra.ExactArgs(3),
 		Short: "Set gravity delegate keys",
+		Long: `Set a validator's Ethereum and orchestrator addresses. The validator must
+sign over a binary Proto-encoded DelegateKeysSignMsg message. The message contains
+the validator's address and operator account current nonce.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -156,14 +159,17 @@ func CmdSetDelegateKeys() *cobra.Command {
 			if err != nil {
 				return err
 			}
+
 			orcAddr, err := sdk.AccAddressFromBech32(args[1])
 			if err != nil {
 				return err
 			}
+
 			ethAddr, err := parseContractAddress(args[2])
 			if err != nil {
 				return err
 			}
+
 			ethsig, err := parseContractAddress(args[3])
 			if err != nil {
 				return err
@@ -173,6 +179,7 @@ func CmdSetDelegateKeys() *cobra.Command {
 			if err = msg.ValidateBasic(); err != nil {
 				return err
 			}
+
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
