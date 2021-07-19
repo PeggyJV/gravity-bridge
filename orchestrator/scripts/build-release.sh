542a14079dbba514bc45625f082bac9cc90f658a
Levi Cook <levicook@gmail.com>
2021-07-19 18:52:02 +0000

    let's release gorc too

--- orchestrator/scripts/build-release.sh
+++ orchestrator/scripts/build-release.sh
@@ -6,11 +6,13 @@ cross build --target=aarch64-unknown-linux-musl --release  --all
 mkdir -p bins
 
 cp target/x86_64-unknown-linux-musl/release/client bins/
+cp target/x86_64-unknown-linux-musl/release/gorc bins/
 cp target/x86_64-unknown-linux-musl/release/orchestrator bins/
-cp target/x86_64-unknown-linux-musl/release/relayer bins/
 cp target/x86_64-unknown-linux-musl/release/register-delegate-keys bins/
+cp target/x86_64-unknown-linux-musl/release/relayer bins/
 
 cp target/aarch64-unknown-linux-musl/release/client bins/client-arm
+cp target/aarch64-unknown-linux-musl/release/gorc bins/gorc-arm
 cp target/aarch64-unknown-linux-musl/release/orchestrator bins/orchestrator-arm
-cp target/aarch64-unknown-linux-musl/release/relayer bins/relayer-arm
 cp target/aarch64-unknown-linux-musl/release/register-delegate-keys bins/register-delegate-keys-arm
+cp target/aarch64-unknown-linux-musl/release/relayer bins/relayer-arm
