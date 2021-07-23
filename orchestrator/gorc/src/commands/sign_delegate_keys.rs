b7071c6f36992b4656ab435c67925df155fe9019
Hannydevelop <ukpaiugochiibem @gmail.com>
2021-07-23 09:25:35 +0100

    making nonce optional

--- orchestrator/gorc/src/commands/sign_delegate_keys.rs
+++ orchestrator/gorc/src/commands/sign_delegate_keys.rs
@@ -1,6 +1,9 @@
 use crate::application::APP;
 use abscissa_core::{Application, Command, Options, Runnable};
+use cosmos_gravity::query as QueryValSet;
 use gravity_proto::gravity as proto;
+use gravity_proto::gravity::query_client::QueryClient as GravityQueryClient;
+use tonic::transport::Channel;
 
 #[derive(Command, Debug, Default, Options)]
 pub struct SignDelegateKeysCmd {
@@ -8,7 +11,7 @@ pub struct SignDelegateKeysCmd {
     // #[options(free, help = "sign-delegate-key [ethereum-key-name] [validator-address] (event-nonce)")]
     #[options(
         free,
-        help = "sign-delegate-key [ethereum-key-name] [validator-address] [nonce]"
+        help = "sign-delegate-key [ethereum-key-name] [validator-address] (nonce)"
     )]
     pub args: Vec<String>,
 }
@@ -16,28 +19,35 @@ pub struct SignDelegateKeysCmd {
 impl Runnable for SignDelegateKeysCmd {
     fn run(&self) {
         let config = APP.config();
+        abscissa_tokio::run(&APP, async {
+            let name = self.args.get(0).expect("ethereum-key-name is required");
+            let key = config.load_clarity_key(name.clone());
 
-        let name = self.args.get(0).expect("ethereum-key-name is required");
-        let key = config.load_clarity_key(name.clone());
+            let val = self.args.get(1).expect("validator-address is required");
+            // TODO(levi) ensure this is a valoper address for the next release
 
-        let val = self.args.get(1).expect("validator-address is required");
-        // TODO(levi) ensure this is a valoper address for the next release
+            let nonce = self.args.get(2).expect("nonce is required");
+            let nonce = nonce.parse().expect("could not parse nonce");
 
-        let nonce = self.args.get(2).expect("nonce is required");
-        let nonce = nonce.parse().expect("could not parse nonce");
+            let client = &mut config.cosmos.grpc.clone();
+            let client = client
+                .parse::<GravityQueryClient<Channel>>()
+                .expect("Could not parse derivation path");
 
-        let msg = proto::DelegateKeysSignMsg {
-            validator_address: val.clone(),
-            nonce,
-        };
+            let valset = QueryValSet::get_valset(&mut client, nonce);
 
-        let size = prost::Message::encoded_len(&msg);
-        let mut buf = bytes::BytesMut::with_capacity(size);
-        prost::Message::encode(&msg, &mut buf)
-            .expect("Failed to encode DelegateKeysSignMsg!");
+            let msg = proto::DelegateKeysSignMsg {
+                validator_address: val.clone(),
+                nonce,
+            };
 
-        let signature = key.sign_ethereum_msg(&buf);
+            let size = prost::Message::encoded_len(&msg);
+            let mut buf = bytes::BytesMut::with_capacity(size);
+            prost::Message::encode(&msg, &mut buf).expect("Failed to encode DelegateKeysSignMsg!");
 
-        println!("{}", signature);
+            let signature = key.sign_ethereum_msg(&buf);
+
+            println!("{}", signature);
+        });
     }
 }
