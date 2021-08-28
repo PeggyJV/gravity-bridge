141267b9bbdccbdd367c5f30a31082f3b3fa269d
Zaki Manian <zaki@manian.org>
2021-08-28 10:27:01 -0700

    formatting

--- orchestrator/gorc/src/commands/tx/cosmos.rs
+++ orchestrator/gorc/src/commands/tx/cosmos.rs
@@ -3,7 +3,7 @@
 use crate::{application::APP, prelude::*, utils::*};
 use abscissa_core::{Command, Options, Runnable};
 use clarity::{Address as EthAddress, Uint256};
-use cosmos_gravity::send::{send_to_eth};
+use cosmos_gravity::send::send_to_eth;
 use deep_space::{coin::Coin, private_key::PrivateKey as CosmosPrivateKey};
 use gravity_proto::gravity::DenomToErc20Request;
 use gravity_utils::connection_prep::{check_for_fee_denom, create_rpc_connections};
