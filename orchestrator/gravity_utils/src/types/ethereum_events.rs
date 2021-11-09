8b37d8b44e64503476e154246ddd7509463811db
Eric Bolten <eric@bolten.dev>
2021-11-08 22:39:32 -0800

    Complete conversion of ethereum_events.rs
    All event types are now using abigen versions. Additionally, it appears
    that the conversion of LogicCallExecutedEvent from a log was just marked
    as "unimplemented" but now has been added as well. Also rewrote the
    event nonce filtering using iterators and adaptors. There's probably a
    way of further using traits to reduce code duplication here but it's not
    a huge issue that it's not super DRY right now.

--- orchestrator/gravity_utils/src/types/ethereum_events.rs
+++ orchestrator/gravity_utils/src/types/ethereum_events.rs
@@ -26,11 +26,24 @@ abigen!(
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
+// given a Log retrieved by querying the Ethereum chain, decode it into one of
+// the types we are generating using abigen! above for the Gravity contract
 fn log_to_ethers_event<T: EthLogDecode>(log: &Log) -> Result<T, ethers::abi::Error> {
     T::decode_log(&RawLog {
         topics: log.topics,
         data: log.data.to_vec(),
     }).map_err(From::from)
+}
+
+// our event model structs use U256 to represent block height, but Logs provide it
+// to us as a U64 (strangely, no direct conversion from U64, so we have to do this type dance)
+fn block_height_from_log(log: &Log) -> Result<U256, GravityError> {
+    match log.block_number.clone() {
+        Some(block_height) => Ok(U256::from(block_height.as_u64())),
+        None => Err(GravityError::InvalidEventLogError(
+            "Log does not have block number, we only search logs already in blocks?".to_string())
+        )
+    }
 }
 
 /// A parsed struct representing the Ethereum event fired by the Gravity contract
@@ -70,22 +83,14 @@ impl ValsetUpdatedEvent {
             );
         }
 
-        let block_height = if let Some(bn) = input.block_number.clone() {
-            bn
-        } else {
-            return Err(GravityError::InvalidEventLogError(
-                "Log does not have block number, we only search logs already in blocks?"
-                    .to_string(),
-            ));
-        };
-
         Ok(ValsetUpdatedEvent {
             valset_nonce: event.new_valset_nonce,
             event_nonce: event.event_nonce,
-            block_height: U256::from(block_height.as_u64()),
+            block_height: block_height_from_log(&input)?,
             members: validators,
         })
     }
+
     pub fn from_logs(input: &[Log]) -> Result<Vec<ValsetUpdatedEvent>, GravityError> {
         let mut res = Vec::new();
         for item in input {
@@ -93,16 +98,14 @@ impl ValsetUpdatedEvent {
         }
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
-        let mut ret = Vec::new();
-        for item in input {
-            if item.event_nonce > event_nonce.into() {
-                ret.push(item.clone())
-            }
-        }
-        ret
+        input.iter()
+            .filter(|item| item.event_nonce > event_nonce.into())
+            .map(|item| item.clone())
+            .collect()
     }
 }
 
@@ -112,55 +115,30 @@ impl ValsetUpdatedEvent {
 pub struct TransactionBatchExecutedEvent {
     /// the nonce attached to the transaction batch that follows
     /// it throughout it's lifecycle
-    pub batch_nonce: Uint256,
+    pub batch_nonce: U256,
     /// The block height this event occurred at
-    pub block_height: Uint256,
+    pub block_height: U256,
     /// The ERC20 token contract address for the batch executed, since batches are uniform
     /// in token type there is only one
     pub erc20: EthAddress,
     /// the event nonce representing a unique ordering of events coming out
     /// of the Gravity solidity contract. Ensuring that these events can only be played
     /// back in order
-    pub event_nonce: Uint256,
+    pub event_nonce: U256,
 }
 
 impl TransactionBatchExecutedEvent {
     pub fn from_log(input: &Log) -> Result<TransactionBatchExecutedEvent, GravityError> {
-        if let (Some(batch_nonce_data), Some(erc20_data)) =
-            (input.topics.get(1), input.topics.get(2))
-        {
-            let batch_nonce = Uint256::from_bytes_be(batch_nonce_data);
-            let erc20 = EthAddress::from_slice(&erc20_data[12..32])?;
-            let event_nonce = Uint256::from_bytes_be(&input.data);
-            let block_height = if let Some(bn) = input.block_number.clone() {
-                bn
-            } else {
-                return Err(GravityError::InvalidEventLogError(
-                    "Log does not have block number, we only search logs already in blocks?"
-                        .to_string(),
-                ));
-            };
-            if event_nonce > u64::MAX.into()
-                || batch_nonce > u64::MAX.into()
-                || block_height > u64::MAX.into()
-            {
-                Err(GravityError::InvalidEventLogError(
-                    "Event nonce overflow, probably incorrect parsing".to_string(),
-                ))
-            } else {
-                Ok(TransactionBatchExecutedEvent {
-                    batch_nonce,
-                    block_height,
-                    erc20,
-                    event_nonce,
-                })
-            }
-        } else {
-            Err(GravityError::InvalidEventLogError(
-                "Too few topics".to_string(),
-            ))
-        }
+        let event: TransactionBatchExecutedEventFilter = log_to_ethers_event(input)?;
+
+        Ok(TransactionBatchExecutedEvent {
+            batch_nonce: event.batch_nonce,
+            block_height: block_height_from_log(&input)?,
+            erc20: event.token,
+            event_nonce: event.event_nonce,
+        })
     }
+
     pub fn from_logs(input: &[Log]) -> Result<Vec<TransactionBatchExecutedEvent>, GravityError> {
         let mut res = Vec::new();
         for item in input {
@@ -168,16 +146,14 @@ impl TransactionBatchExecutedEvent {
         }
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
-        let mut ret = Vec::new();
-        for item in input {
-            if item.event_nonce > event_nonce.into() {
-                ret.push(item.clone())
-            }
-        }
-        ret
+        input.iter()
+            .filter(|item| item.event_nonce > event_nonce.into())
+            .map(|item| item.clone())
+            .collect()
     }
 }
 
@@ -192,58 +168,28 @@ pub struct SendToCosmosEvent {
     /// The Cosmos destination
     pub destination: CosmosAddress,
     /// The amount of the erc20 token that is being sent
-    pub amount: Uint256,
+    pub amount: U256,
     /// The transaction's nonce, used to make sure there can be no accidental duplication
-    pub event_nonce: Uint256,
+    pub event_nonce: U256,
     /// The block height this event occurred at
-    pub block_height: Uint256,
+    pub block_height: U256,
 }
 
 impl SendToCosmosEvent {
     pub fn from_log(input: &Log, prefix: &str) -> Result<SendToCosmosEvent, GravityError> {
-        let topics = (
-            input.topics.get(1),
-            input.topics.get(2),
-            input.topics.get(3),
-        );
-        if let (Some(erc20_data), Some(sender_data), Some(destination_data)) = topics {
-            let erc20 = EthAddress::from_slice(&erc20_data[12..32])?;
-            let sender = EthAddress::from_slice(&sender_data[12..32])?;
-            // this is required because deep_space requires a fixed length slice to
-            // create an address from bytes.
-            let mut c_address_bytes: [u8; 20] = [0; 20];
-            c_address_bytes.copy_from_slice(&destination_data[12..32]);
-            let destination = CosmosAddress::from_bytes(c_address_bytes, prefix).unwrap();
-            let amount = Uint256::from_bytes_be(&input.data[..32]);
-            let event_nonce = Uint256::from_bytes_be(&input.data[32..]);
-            let block_height = if let Some(bn) = input.block_number.clone() {
-                bn
-            } else {
-                return Err(GravityError::InvalidEventLogError(
-                    "Log does not have block number, we only search logs already in blocks?"
-                        .to_string(),
-                ));
-            };
-            if event_nonce > u64::MAX.into() || block_height > u64::MAX.into() {
-                Err(GravityError::InvalidEventLogError(
-                    "Event nonce overflow, probably incorrect parsing".to_string(),
-                ))
-            } else {
-                Ok(SendToCosmosEvent {
-                    erc20,
-                    sender,
-                    destination,
-                    amount,
-                    event_nonce,
-                    block_height,
-                })
-            }
-        } else {
-            Err(GravityError::InvalidEventLogError(
-                "Too few topics".to_string(),
-            ))
-        }
+        let event: SendToCosmosEventFilter = log_to_ethers_event(input)?;
+
+        // TODO(bolten): verify this method of retrieval for Cosmos addresses works as expected
+        Ok(SendToCosmosEvent {
+            erc20: event.token_contract,
+            sender: event.sender,
+            destination: CosmosAddress::from_slice(&event.destination[12..32], prefix)?,
+            amount: event.amount,
+            event_nonce: event.event_nonce,
+            block_height: block_height_from_log(&input)?,
+        })
     }
+
     pub fn from_logs(
         input: &[Log],
         prefix: &str,
@@ -254,16 +200,14 @@ impl SendToCosmosEvent {
         }
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
-        let mut ret = Vec::new();
-        for item in input {
-            if item.event_nonce > event_nonce.into() {
-                ret.push(item.clone())
-            }
-        }
-        ret
+        input.iter()
+            .filter(|item| item.event_nonce > event_nonce.into())
+            .map(|item| item.clone())
+            .collect()
     }
 }
 
@@ -283,130 +227,25 @@ pub struct Erc20DeployedEvent {
     pub symbol: String,
     /// The number of decimals required to represent the smallest unit of this token
     pub decimals: u8,
-    pub event_nonce: Uint256,
-    pub block_height: Uint256,
+    pub event_nonce: U256,
+    pub block_height: U256,
 }
 
 impl Erc20DeployedEvent {
     pub fn from_log(input: &Log) -> Result<Erc20DeployedEvent, GravityError> {
-        let token_contract = input.topics.get(1);
-        if let Some(new_token_contract_data) = token_contract {
-            let erc20 = EthAddress::from_slice(&new_token_contract_data[12..32])?;
-            let index_start = 3 * 32;
-            let index_end = index_start + 32;
-            let decimals = Uint256::from_bytes_be(&input.data[index_start..index_end]);
-            if decimals > u8::MAX.into() {
-                return Err(GravityError::InvalidEventLogError(
-                    "Decimals overflow, probably incorrect parsing".to_string(),
-                ));
-            }
-            let decimals: u8 = decimals.to_string().parse().unwrap();
+        let event: Erc20DeployedEventFilter = log_to_ethers_event(input)?;
 
-            let index_start = 4 * 32;
-            let index_end = index_start + 32;
-            let nonce = Uint256::from_bytes_be(&input.data[index_start..index_end]);
-            if nonce > u64::MAX.into() {
-                return Err(GravityError::InvalidEventLogError(
-                    "Nonce overflow, probably incorrect parsing".to_string(),
-                ));
-            }
-
-            let index_start = 5 * 32;
-            let index_end = index_start + 32;
-            let denom_len = Uint256::from_bytes_be(&input.data[index_start..index_end]);
-            // it's not probable that we have 4+ gigabytes of event data
-            if denom_len > u32::MAX.into() {
-                return Err(GravityError::InvalidEventLogError(
-                    "denom length overflow, probably incorrect parsing".to_string(),
-                ));
-            }
-            let denom_len: usize = denom_len.to_string().parse().unwrap();
-            let index_start = 6 * 32;
-            let index_end = index_start + denom_len;
-            let denom = String::from_utf8(input.data[index_start..index_end].to_vec());
-            trace!("Denom {:?}", denom);
-            if denom.is_err() {
-                return Err(GravityError::InvalidEventLogError(format!(
-                    "{:?} is not valid utf8, probably incorrect parsing",
-                    denom
-                )));
-            }
-            let denom = denom.unwrap();
-
-            // beyond this point we are parsing strings placed
-            // after a variable length string and we will need to compute offsets
-
-            // this trick computes the next 32 byte (256 bit) word index, then multiplies by
-            // 32 to get the bytes offset, this is required since we have dynamic length types but
-            // the next entry always starts on a round 32 byte word.
-            let index_start = ((index_end + 31) / 32) * 32;
-            let index_end = index_start + 32;
-            let erc20_name_len = Uint256::from_bytes_be(&input.data[index_start..index_end]);
-            // it's not probable that we have 4+ gigabytes of event data
-            if erc20_name_len > u32::MAX.into() {
-                return Err(GravityError::InvalidEventLogError(
-                    "ERC20 Name length overflow, probably incorrect parsing".to_string(),
-                ));
-            }
-            let erc20_name_len: usize = erc20_name_len.to_string().parse().unwrap();
-            let index_start = index_end;
-            let index_end = index_start + erc20_name_len;
-            let erc20_name = String::from_utf8(input.data[index_start..index_end].to_vec());
-            if erc20_name.is_err() {
-                return Err(GravityError::InvalidEventLogError(format!(
-                    "{:?} is not valid utf8, probably incorrect parsing",
-                    erc20_name
-                )));
-            }
-            trace!("ERC20 Name {:?}", erc20_name);
-            let erc20_name = erc20_name.unwrap();
-
-            let index_start = ((index_end + 31) / 32) * 32;
-            let index_end = index_start + 32;
-            let symbol_len = Uint256::from_bytes_be(&input.data[index_start..index_end]);
-            // it's not probable that we have 4+ gigabytes of event data
-            if symbol_len > u32::MAX.into() {
-                return Err(GravityError::InvalidEventLogError(
-                    "Symbol length overflow, probably incorrect parsing".to_string(),
-                ));
-            }
-            let symbol_len: usize = symbol_len.to_string().parse().unwrap();
-            let index_start = index_end;
-            let index_end = index_start + symbol_len;
-            let symbol = String::from_utf8(input.data[index_start..index_end].to_vec());
-            trace!("Symbol {:?}", symbol);
-            if symbol.is_err() {
-                return Err(GravityError::InvalidEventLogError(format!(
-                    "{:?} is not valid utf8, probably incorrect parsing",
-                    symbol
-                )));
-            }
-            let symbol = symbol.unwrap();
-
-            let block_height = if let Some(bn) = input.block_number.clone() {
-                bn
-            } else {
-                return Err(GravityError::InvalidEventLogError(
-                    "Log does not have block number, we only search logs already in blocks?"
-                        .to_string(),
-                ));
-            };
-
-            Ok(Erc20DeployedEvent {
-                cosmos_denom: denom,
-                name: erc20_name,
-                decimals,
-                event_nonce: nonce,
-                erc20_address: erc20,
-                symbol,
-                block_height,
-            })
-        } else {
-            Err(GravityError::InvalidEventLogError(
-                "Too few topics".to_string(),
-            ))
-        }
+        Ok(Erc20DeployedEvent {
+            cosmos_denom: event.cosmos_denom,
+            erc20_address: event.token_contract,
+            name: event.name,
+            symbol: event.symbol,
+            decimals: event.decimals,
+            event_nonce: event.event_nonce,
+            block_height: block_height_from_log(&input)?,
+        })
     }
+
     pub fn from_logs(input: &[Log]) -> Result<Vec<Erc20DeployedEvent>, GravityError> {
         let mut res = Vec::new();
         for item in input {
@@ -414,16 +253,14 @@ impl Erc20DeployedEvent {
         }
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
-        let mut ret = Vec::new();
-        for item in input {
-            if item.event_nonce > event_nonce.into() {
-                ret.push(item.clone())
-            }
-        }
-        ret
+        input.iter()
+            .filter(|item| item.event_nonce > event_nonce.into())
+            .map(|item| item.clone())
+            .collect()
     }
 }
 /// A parsed struct representing the Ethereum event fired when someone uses the Gravity
@@ -431,16 +268,25 @@ impl Erc20DeployedEvent {
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
 pub struct LogicCallExecutedEvent {
     pub invalidation_id: Vec<u8>,
-    pub invalidation_nonce: Uint256,
+    pub invalidation_nonce: U256,
     pub return_data: Vec<u8>,
-    pub event_nonce: Uint256,
-    pub block_height: Uint256,
+    pub event_nonce: U256,
+    pub block_height: U256,
 }
 
 impl LogicCallExecutedEvent {
-    pub fn from_log(_input: &Log) -> Result<LogicCallExecutedEvent, GravityError> {
-        unimplemented!("foo")
+    pub fn from_log(input: &Log) -> Result<LogicCallExecutedEvent, GravityError> {
+        let event: LogicCallEventFilter = log_to_ethers_event(input)?;
+
+        Ok(LogicCallExecutedEvent {
+            invalidation_id: event.invalidation_id.into(),
+            invalidation_nonce: event.invalidation_nonce,
+            return_data: event.return_data.into(),
+            event_nonce: event.event_nonce,
+            block_height: block_height_from_log(&input)?,
+        })
     }
+
     pub fn from_logs(input: &[Log]) -> Result<Vec<LogicCallExecutedEvent>, GravityError> {
         let mut res = Vec::new();
         for item in input {
@@ -448,16 +294,14 @@ impl LogicCallExecutedEvent {
         }
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
-        let mut ret = Vec::new();
-        for item in input {
-            if item.event_nonce > event_nonce.into() {
-                ret.push(item.clone())
-            }
-        }
-        ret
+        input.iter()
+            .filter(|item| item.event_nonce > event_nonce.into())
+            .map(|item| item.clone())
+            .collect()
     }
 }
 
