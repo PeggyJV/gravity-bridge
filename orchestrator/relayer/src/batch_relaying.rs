555af297ff1c91b060860c65ba5ec01b0bd6a3d0
Collin Brittain <collinbrittain@gmail.com>
2021-11-15 16:27:48 -0600

    fmt and fixed some mistakes from rebase

--- orchestrator/relayer/src/batch_relaying.rs
+++ orchestrator/relayer/src/batch_relaying.rs
@@ -10,6 +10,7 @@ use gravity_proto::gravity::query_client::QueryClient as GravityQueryClient;
 use gravity_utils::message_signatures::encode_tx_batch_confirm_hashed;
 use gravity_utils::types::Valset;
 use gravity_utils::types::{BatchConfirmResponse, TransactionBatch};
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
 use std::collections::HashMap;
 use std::time::Duration;
 use tonic::transport::Channel;
@@ -112,7 +113,8 @@ async fn get_batches_and_signatures(
     // older batches so that we don't invalidate newer batches
     possible_batches.par_iter_mut().for_each(|(_key, value)| {
         value.reverse();
-    }
+    });
+
     possible_batches
 }
 
