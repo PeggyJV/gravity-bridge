265304d23b56d020a45f0e1d697df67295bc498c
hannydevelop <ukpaiugochiibem@gmail.com>
2021-11-19 11:51:31 +0000

    eradicated implementation for display format

--- orchestrator/gravity_utils/src/error.rs
+++ orchestrator/gravity_utils/src/error.rs
@@ -4,7 +4,6 @@ use clarity::Error as ClarityError;
 use deep_space::error::AddressError as CosmosAddressError;
 use deep_space::error::CosmosGrpcError;
 use num_bigint::ParseBigIntError;
-use std::fmt::{self, Debug};
 use thiserror::Error;
 use tokio::time::error::Elapsed;
 use tonic::Status;
@@ -54,37 +53,6 @@ pub enum GravityError {
 
     #[error("Failed to parse big integer {0}")]
     ParseBigIntError(ParseBigIntError)
-}
-
-impl fmt::Display for GravityError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            GravityError::GravityGrpcError(val) => write!(f, "Gravity gRPC error {}", val),
-            GravityError::CosmosGrpcError(val) => write!(f, "Cosmos gRPC error {}", val),
-            GravityError::InvalidBigInt(val) => {
-                write!(f, "Got invalid BigInt from cosmos! {}", val)
-            }
-            GravityError::CosmosAddressError(val) => write!(f, "Cosmos Address error {}", val),
-            GravityError::EthereumRestError(val) => write!(f, "Ethereum REST error {}", val),
-            GravityError::InvalidOptionsError(val) => {
-                write!(f, "Invalid TX options for this call {}", val)
-            }
-            GravityError::InvalidBridgeStateError(val) => {
-                write!(f, "Invalid bridge state! {}", val)
-            }
-            GravityError::FailedToUpdateValset => write!(f, "ValidatorSetUpdate Failed!"),
-            GravityError::TimeoutError => write!(f, "Operation timed out!"),
-            GravityError::ClarityError(val) => write!(f, "Clarity Error {}", val),
-            GravityError::InvalidEventLogError(val) => write!(f, "InvalidEvent: {}", val),
-            GravityError::EthereumContractError(val) => {
-                write!(f, "Contract operation failed: {}", val)
-            }
-            GravityError::InsufficientVotingPowerToPass(val) => {
-                write!(f, "{}", val)
-            }
-            GravityError::ParseBigIntError(val) => write!(f, "Failed to parse big integer {}", val),
-        }
-    }
 }
 
 impl std::error::Error for GravityError {}
