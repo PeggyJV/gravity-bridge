09d4a504d6db2bd96f5f8457b60123b366018de3
Zaki Manian <zaki@iqlusion.io>
2021-12-03 09:42:23 -0800

    update reentrancyguard path

--- solidity/contracts/Gravity.sol
+++ solidity/contracts/Gravity.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./CosmosToken.sol";
 
 pragma experimental ABIEncoderV2;
