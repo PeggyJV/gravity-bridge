4c747d0c3a3a0adac2874f6d869053ced1a4c95d
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2022-01-05 20:34:04 +0100

    check for tokens in bank

--- integration_tests/happy_path_test.go
+++ integration_tests/happy_path_test.go
@@ -1,15 +1,20 @@
 package integration_tests
 
 import (
+	"context"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/peggyjv/gravity-bridge/module/x/gravity/types"
 )
 
 func (s *IntegrationTestSuite) TestHappyPath() {
 	s.Run("Bring up chain, and test the happy path", func() {
+		const DENOM = "DDS"
+
+		s.T().Logf("approving Gravity to spend ERC 20")
 		err := s.approveERC20()
 		s.Require().NoError(err, "error approving spending balance for the gravity contract on behalf of the first validator")
 
@@ -22,14 +27,46 @@ func (s *IntegrationTestSuite) TestHappyPath() {
 		s.Require().Equal(sdk.NewUint(10000).BigInt(), balance.BigInt(), "balance was %s, expected 10000", balance.String())
 
 		// send from val 0 on eth to val 1 on cosmos
+		s.T().Logf("sending to cosmos")
 		err = s.sendToCosmos(s.chain.validators[1].keyInfo.GetAddress(), sdk.NewInt(200))
 		s.Require().NoError(err, "error sending test denom to cosmos")
+
+		s.Require().Eventuallyf(func() bool {
+			val := s.chain.validators[0]
+			kb, err := val.keyring()
+			s.Require().NoError(err)
+			clientCtx, err := s.chain.clientContext("tcp://localhost:26657", &kb, "val", val.keyInfo.GetAddress())
+			s.Require().NoError(err)
+
+			queryClient := banktypes.NewQueryClient(clientCtx)
+			res, err := queryClient.AllBalances(context.Background(),
+				&banktypes.QueryAllBalancesRequest{
+					Address: s.chain.validators[1].keyInfo.GetAddress().String(),
+				})
+			if err != nil {
+				return false
+			}
+
+			for _, coin := range res.Balances {
+				if coin.Denom == DENOM {
+					if coin.Amount == sdk.NewInt(100) {
+						return true
+					}
+				}
+			}
+
+			s.T().Logf("balance not found, received %v", res.Balances)
+
+			return false
+		}, 105*time.Second, 1*time.Second, "new vote period never seen")
+
+		s.T().Logf("sending to ethereum")
 
 		sendToEthereumMsg := types.NewMsgSendToEthereum(
 			s.chain.validators[1].keyInfo.GetAddress(),
 			s.chain.validators[1].ethereumKey.address,
-			sdk.Coin{Denom: "DDS", Amount: sdk.NewInt(100)},
-			sdk.Coin{Denom: "DDS", Amount: sdk.NewInt(1)},
+			sdk.Coin{Denom: DENOM, Amount: sdk.NewInt(100)},
+			sdk.Coin{Denom: DENOM, Amount: sdk.NewInt(1)},
 		)
 
 		s.Require().Eventuallyf(func() bool {
