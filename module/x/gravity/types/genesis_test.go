459b767eb75a69bf0c810e005a4ddcbfdedb2c07
Eric Bolten <eric@bolten.dev>
2022-01-20 17:40:04 -0800

    Use cosmos addresses without touching sdk config

--- module/x/gravity/types/genesis_test.go
+++ module/x/gravity/types/genesis_test.go
@@ -3,15 +3,10 @@ package types
 import (
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGenesisStateValidate(t *testing.T) {
-	sdk.GetConfig().SetBech32PrefixForValidator("sommvaloper", "sommvaloperpub")
-	sdk.GetConfig().SetBech32PrefixForAccount("somm", "sommpub")
-	sdk.GetConfig().SetBech32PrefixForConsensusNode("sommcons", "sommconspub")
-
 	var nilByteSlice []byte
 	specs := map[string]struct {
 		src    *GenesisState
@@ -31,8 +26,8 @@ func TestGenesisStateValidate(t *testing.T) {
 			Params: DefaultParams(),
 			DelegateKeys: []*MsgDelegateKeys{
 				{
-					ValidatorAddress:    "sommvaloper1w4lcqzsrfgfwrgd386v9n6frj9p66k4xazvsqn",
-					OrchestratorAddress: "somm1ht877l3pgqjkalxwtvmydr65jnde3p4g7vxq5t",
+					ValidatorAddress:    "cosmosvaloper1jpz0ahls2chajf78nkqczdwwuqcu97w6z3plt4",
+					OrchestratorAddress: "cosmos1g0etv93428tvxqftnmj25jn06mz6dtdasj5nz7",
 					EthereumAddress:     "0x494eeff8848254C4fdd5B529FC6E751Ab34597A6",
 					EthSignature:        []byte("xIm9M32dXl7II8c72qXS/x9xVQMbX9sHVkuigxSmspNhSnEc2dLe6YDn/+Yi3VAOwXKh8zUBSHMplQSEuOY1zhw="),
 				},
@@ -42,8 +37,8 @@ func TestGenesisStateValidate(t *testing.T) {
 			Params: DefaultParams(),
 			DelegateKeys: []*MsgDelegateKeys{
 				{
-					ValidatorAddress:    "sommvaloper1w4lcqzsrfgfwrgd386v9n6frj9p66k4xazvsqn",
-					OrchestratorAddress: "somm1ht877l3pgqjkalxwtvmydr65jnde3p4g7vxq5t",
+					ValidatorAddress:    "cosmosvaloper1jpz0ahls2chajf78nkqczdwwuqcu97w6z3plt4",
+					OrchestratorAddress: "cosmos1g0etv93428tvxqftnmj25jn06mz6dtdasj5nz7",
 					EthereumAddress:     "0x494eeff8848254C4fdd5B529FC6E751Ab34597A6",
 					EthSignature:        []byte("unused"),
 				},
@@ -53,8 +48,8 @@ func TestGenesisStateValidate(t *testing.T) {
 			Params: DefaultParams(),
 			DelegateKeys: []*MsgDelegateKeys{
 				{
-					ValidatorAddress:    "sommvaloper1w4lcqzsrfgfwrgd386v9n6frj9p66k4xazvsqn",
-					OrchestratorAddress: "somm1ht877l3pgqjkalxwtvmydr65jnde3p4g7vxq5t",
+					ValidatorAddress:    "cosmosvaloper1jpz0ahls2chajf78nkqczdwwuqcu97w6z3plt4",
+					OrchestratorAddress: "cosmos1g0etv93428tvxqftnmj25jn06mz6dtdasj5nz7",
 					EthereumAddress:     "0x494eeff8848254C4fdd5B529FC6E751Ab34597A6",
 					EthSignature:        nilByteSlice,
 				},
@@ -64,8 +59,8 @@ func TestGenesisStateValidate(t *testing.T) {
 			Params: DefaultParams(),
 			DelegateKeys: []*MsgDelegateKeys{
 				{
-					ValidatorAddress:    "sommvaloper1wrong",
-					OrchestratorAddress: "somm1ht877l3pgqjkalxwtvmydr65jnde3p4g7vxq5t",
+					ValidatorAddress:    "cosmosvaloper1wrong",
+					OrchestratorAddress: "cosmos1g0etv93428tvxqftnmj25jn06mz6dtdasj5nz7",
 					EthereumAddress:     "0x494eeff8848254C4fdd5B529FC6E751Ab34597A6",
 					EthSignature:        []byte("xIm9M32dXl7II8c72qXS/x9xVQMbX9sHVkuigxSmspNhSnEc2dLe6YDn/+Yi3VAOwXKh8zUBSHMplQSEuOY1zhw="),
 				},
@@ -75,8 +70,8 @@ func TestGenesisStateValidate(t *testing.T) {
 			Params: DefaultParams(),
 			DelegateKeys: []*MsgDelegateKeys{
 				{
-					ValidatorAddress:    "sommvaloper1w4lcqzsrfgfwrgd386v9n6frj9p66k4xazvsqn",
-					OrchestratorAddress: "somm1wrong",
+					ValidatorAddress:    "cosmosvaloper1jpz0ahls2chajf78nkqczdwwuqcu97w6z3plt4",
+					OrchestratorAddress: "cosmos1wrong",
 					EthereumAddress:     "0x494eeff8848254C4fdd5B529FC6E751Ab34597A6",
 					EthSignature:        []byte("xIm9M32dXl7II8c72qXS/x9xVQMbX9sHVkuigxSmspNhSnEc2dLe6YDn/+Yi3VAOwXKh8zUBSHMplQSEuOY1zhw="),
 				},
@@ -86,8 +81,8 @@ func TestGenesisStateValidate(t *testing.T) {
 			Params: DefaultParams(),
 			DelegateKeys: []*MsgDelegateKeys{
 				{
-					ValidatorAddress:    "sommvaloper1w4lcqzsrfgfwrgd386v9n6frj9p66k4xazvsqn",
-					OrchestratorAddress: "somm1ht877l3pgqjkalxwtvmydr65jnde3p4g7vxq5t",
+					ValidatorAddress:    "cosmosvaloper1jpz0ahls2chajf78nkqczdwwuqcu97w6z3plt4",
+					OrchestratorAddress: "cosmos1g0etv93428tvxqftnmj25jn06mz6dtdasj5nz7",
 					EthereumAddress:     "0xdeadbeef",
 					EthSignature:        []byte("xIm9M32dXl7II8c72qXS/x9xVQMbX9sHVkuigxSmspNhSnEc2dLe6YDn/+Yi3VAOwXKh8zUBSHMplQSEuOY1zhw="),
 				},
