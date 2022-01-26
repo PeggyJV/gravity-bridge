94b41692fd1b91304d9a67268a7a44d9be5b2b2e
hannydevelop <ukpaiugochiibem@gmail.com>
2022-01-26 07:19:28 +0000

    fixed deploy command

--- orchestrator/gorc/src/commands/deploy.rs
+++ orchestrator/gorc/src/commands/deploy.rs
@@ -1,10 +1,10 @@
 mod erc20;
 use erc20::Erc20;
 
-use abscissa_core::{Clap, Command, Runnable};
+use abscissa_core::{clap::Parser, Command, Runnable};
 
 /// Provides tools for contract deployment
-#[derive(Command, Debug, Clap, Runnable)]
+#[derive(Command, Debug, Parser, Runnable)]
 pub enum DeployCmd {
     Erc20(Erc20),
 }
