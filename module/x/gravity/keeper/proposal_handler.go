5c6d1a5529d2a656fa1c274b2ab3d8e12a1240b9
Eric Bolten <eric@bolten.dev>
2022-03-28 20:42:31 -0700

    Include the bridge fee when subtracting from pool

--- module/x/gravity/keeper/proposal_handler.go
+++ module/x/gravity/keeper/proposal_handler.go
@@ -18,7 +18,7 @@ func (k Keeper) HandleCommunityPoolEthereumSpendProposal(ctx sdk.Context, p *typ
 	// NOTE the community pool isn't a module account, however its coins
 	// are held in the distribution module account. Thus the community pool
 	// must be reduced separately from the createSendToEthereum calls
-	newPool, negative := feePool.CommunityPool.SafeSub(sdk.NewDecCoinsFromCoins(p.Amount))
+	newPool, negative := feePool.CommunityPool.SafeSub(sdk.NewDecCoinsFromCoins(p.Amount, p.BridgeFee))
 	if negative {
 		return distributiontypes.ErrBadDistribution
 	}
