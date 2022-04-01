6f512988dd3b7cda807c95f44eef8039de76d718
Eric Bolten <eric@bolten.dev>
2022-03-31 23:07:36 -0700

    Fix inappropriate uses of GravityCoin

--- module/x/gravity/keeper/cosmos_originated.go
+++ module/x/gravity/keeper/cosmos_originated.go
@@ -71,8 +71,7 @@ func (k Keeper) ERC20ToDenomLookup(ctx sdk.Context, tokenContract common.Address
 	}
 
 	// If it is not in there, it is not a cosmos originated token, turn the ERC20 into a gravity denom
-
-	return false, types.NewERC20Token(0, tokenContract).GravityCoin().Denom
+	return false, types.GravityDenom(tokenContract)
 }
 
 // iterateERC20ToDenom iterates over erc20 to denom relations
