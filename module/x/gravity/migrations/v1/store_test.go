8ab92d6daa78898b7ff61bc99056c506d08bb448
Eric Bolten <eric@bolten.dev>
2022-04-04 23:10:55 -0700

    Make sure other cctx fields remained the same

--- module/x/gravity/migrations/v1/store_test.go
+++ module/x/gravity/migrations/v1/store_test.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/peggyjv/gravity-bridge/module/x/gravity/keeper"
 	"github.com/peggyjv/gravity-bridge/module/x/gravity/types"
@@ -40,14 +41,19 @@ func TestMigrateContractCallTxTimeout(t *testing.T) {
 
 	ctx.KVStore(storeKey).Set([]byte{LastEthereumBlockHeightKey}, cdc.MustMarshal(latestEthereumBlockHeight))
 
+	erc20Token := types.ERC20Token{
+		Contract: tokenContractString,
+		Amount:   sdk.NewInt(1),
+	}
+
 	otx := &types.ContractCallTx{
 		InvalidationNonce: uint64(1),
 		InvalidationScope: []byte("test-scope"),
 		Address:           "0x2a24af0501a534fca004ee1bd667b783f205a546",
 		Payload:           []byte("payload"),
 		Timeout:           uint64(1000),
-		Tokens:            []types.ERC20Token{},
-		Fees:              []types.ERC20Token{},
+		Tokens:            []types.ERC20Token{erc20Token},
+		Fees:              []types.ERC20Token{erc20Token},
 		Height:            uint64(100),
 	}
 
@@ -69,5 +75,12 @@ func TestMigrateContractCallTxTimeout(t *testing.T) {
 	assert.NoError(t, err)
 
 	cctx, _ := updatedOtx.(*types.ContractCallTx)
-	assert.Equal(t, cctx.Timeout, uint64(1800))
+	assert.Equal(t, cctx.InvalidationNonce, uint64(1))
+	assert.Equal(t, cctx.InvalidationScope, []byte("test-scope"))
+	assert.Equal(t, cctx.Address, "0x2a24af0501a534fca004ee1bd667b783f205a546")
+	assert.Equal(t, cctx.Payload, []byte("payload"))
+	assert.Equal(t, cctx.Timeout, uint64(1800)) // migration added 800 to the latest height of 1000
+	assert.Equal(t, cctx.Tokens, []types.ERC20Token{erc20Token})
+	assert.Equal(t, cctx.Fees, []types.ERC20Token{erc20Token})
+	assert.Equal(t, cctx.Height, uint64(100))
 }
