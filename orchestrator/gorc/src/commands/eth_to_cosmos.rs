5a81327a79d0f76e1074eafe1ae880d372c7928e
hannydevelop <ukpaiugochiibem@gmail.com>
2022-04-06 12:46:18 +0000

    worked on eth_to_cosmos command

--- orchestrator/gorc/src/commands/eth_to_cosmos.rs
+++ orchestrator/gorc/src/commands/eth_to_cosmos.rs
@@ -15,24 +15,49 @@ const TIMEOUT: Duration = Duration::from_secs(60);
 
 /// This command send Ethereum to Cosmos
 #[derive(Command, Debug, Default, Parser)]
+#[clap(
+    long_about = "DESCRIPTION \n\n Send Eth token to Cosmos chain.\n This command sends Eth token to the Cosmos chain via the Gravity bridge. \n It takes the tx amount, Eth keyname, contract address, Cosmos token destination, number of times \n and the ERC20 token contract address."
+)]
 pub struct EthToCosmosCmd {
-    pub args: Vec<String>,
+    /// Erc20 contract address.
+    #[clap(short = 'E', long)]
+    erc20_address: String,
+
+    /// Tx amount.
+    #[clap(short, long)]
+    init_amount: String,
+
+    /// Eth keyname.
+    #[clap(short, long)]
+    ethereum_key: String,
+
+    /// Cosmos address
+    #[clap(short, long)]
+    cosmos_dest: String,
+
+    /// The number of times transactions should repeat itself, default is 1.
+    #[clap(short, long, default_value = "1")]
+    times: String,
+
+    /// Contract address.
+    #[clap(short = 'C', long)]
+    contract_address: String,
 }
 
 impl Runnable for EthToCosmosCmd {
     fn run(&self) {
         let config = APP.config();
-        let erc20_address = self.args.get(0).expect("erc20 address is required");
+        let erc20_address = self.erc20_address.clone();
         let erc20_address: EthAddress = erc20_address
             .parse()
             .expect("Invalid ERC20 contract address!");
 
-        let ethereum_key = self.args.get(1).expect("key is required");
-        let ethereum_wallet = config.load_ethers_wallet(ethereum_key.clone());
+        let ethereum_wallet = config.load_ethers_wallet(self.ethereum_key.clone());
 
-        let contract_address = self.args.get(2).expect("contract address is required");
-        let contract_address: EthAddress =
-            contract_address.parse().expect("Invalid contract address!");
+        let contract_address: EthAddress = self
+            .contract_address
+            .parse()
+            .expect("Invalid contract address!");
 
         let cosmos_prefix = config.cosmos.prefix.trim();
         let eth_rpc = config.ethereum.rpc.trim();
@@ -56,22 +81,20 @@ impl Runnable for EthToCosmosCmd {
             let eth_client =
                 SignerMiddleware::new(provider, ethereum_wallet.clone().with_chain_id(chain_id));
             let eth_client = Arc::new(eth_client);
-            let cosmos_dest = self.args.get(3).expect("cosmos destination is required");
-            let cosmos_dest: CosmosAddress = cosmos_dest.parse().unwrap();
+            let cosmos_dest: CosmosAddress = self.cosmos_dest.parse().unwrap();
             let ethereum_address = eth_client.address();
             check_for_eth(ethereum_address, eth_client.clone()).await;
-
-            let init_amount = self.args.get(4).expect("amount is required");
-            let amount = U256::from_dec_str(init_amount).expect("cannot parse amount as U256");
+            let amount =
+                U256::from_dec_str(&self.init_amount).expect("cannot parse amount as U256");
 
             let erc20_balance =
                 get_erc20_balance(erc20_address, ethereum_address, eth_client.clone())
                     .await
                     .expect("Failed to get balance, check ERC20 contract address");
 
-            let times = self.args.get(5).expect("times is required");
+            let times = self.times.clone();
             let times_usize = times.parse::<usize>().expect("cannot parse times as usize");
-            let times_u256 = U256::from_dec_str(times).expect("cannot parse times as U256");
+            let times_u256 = U256::from_dec_str(&times).expect("cannot parse times as U256");
 
             if erc20_balance == 0u8.into() {
                 panic!(
@@ -89,7 +112,7 @@ impl Runnable for EthToCosmosCmd {
             for _ in 0..times_usize {
                 println!(
                     "Sending {} / {} to Cosmos from {} to {}",
-                    init_amount.parse::<f64>().unwrap(),
+                    self.init_amount.parse::<f64>().unwrap(),
                     erc20_address,
                     ethereum_address,
                     cosmos_dest
