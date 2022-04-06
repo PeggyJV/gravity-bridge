0de425da4555c207d3cf2b95160ae2a478098265
Eric Bolten <eric@bolten.dev>
2022-04-06 01:54:26 -0700

    Remove unused mod account names in test_common

--- module/x/gravity/keeper/test_common.go
+++ module/x/gravity/keeper/test_common.go
@@ -369,7 +369,6 @@ func CreateTestEnv(t *testing.T) TestInput {
 	totalSupply := sdk.NewCoins(sdk.NewInt64Coin("stake", 100000000))
 
 	// set up initial accounts
-	modAccNames := make(map[string]string)
 	for name, perms := range maccPerms {
 		mod := authtypes.NewEmptyModuleAccount(name, perms...)
 		if name == stakingtypes.NotBondedPoolName {
@@ -381,7 +380,6 @@ func CreateTestEnv(t *testing.T) TestInput {
 		}
 
 		accountKeeper.SetModuleAccount(ctx, mod)
-		modAccNames[authtypes.NewModuleAddress(name).String()] = name
 	}
 
 	// receiver/sender module account maps for the bridge
