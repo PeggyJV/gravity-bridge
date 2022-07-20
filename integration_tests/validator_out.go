72155722cb908c87bfab69f5083b991d35f2424c
hannydevelop <ukpaiugochiibem@gmail.com>
2022-07-20 07:42:58 +0100

    creating template for validator out test

---
+++ integration_tests/validator_out.go
@@ -0,0 +1,3 @@
+// Validator out tests a validator that is not running the mandatory Ethereum node. This validator will be slashed and the bridge will remain functioning.
+
+package integration_tests
