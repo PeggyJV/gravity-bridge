927e2e5f6e977f1b52b24c58156d1b64bc4998ae
hannydevelop <ukpaiugochiibem@gmail.com>
2022-07-21 09:41:05 +0100

    layout for validator out ready

--- integration_tests/validator_out.go
+++ integration_tests/validator_out.go
@@ -1,3 +1,18 @@
+package integration_tests
+
 // Validator out tests a validator that is not running the mandatory Ethereum node. This validator will be slashed and the bridge will remain functioning.
 
-package integration_tests
+// Start the chain with validators
+func (s *IntegrationTestSuite) TestValidatorOut() {
+	s.Run("Bring up chain, and test the valset update", func() {})
+}
+
+// Check that there's no slashing and validator orchestrator are working fine.
+
+// Stop a validator's orchestrator.
+
+// Check that the chain is still running
+
+// Run an ethereum tx transaction
+
+// Check that the validator has been slashed.
