c4f71614150aa8f39f2d673044990d1063d08efe
hannydevelop <ukpaiugochiibem@gmail.com>
2022-07-29 18:02:24 +0100

    made some changes

--- integration_tests/validator_out.go
+++ integration_tests/validator_out.go
@@ -6,6 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	gravity "github.com/peggyjv/gravity-bridge/module/v2/x/gravity/types"
 )
 
 // Validator out tests a validator that is not running the mandatory Ethereum node. This validator will be slashed and the bridge will remain functioning.
@@ -24,6 +25,18 @@ func (s *IntegrationTestSuite) TestValidatorOut() {
 
 		clientCtx, err := s.chain.clientContext("tcp://localhost:26657", keyring, "orch", orchKey.keyInfo.GetAddress())
 		s.Require().NoError(err)
+
+		// Check jail status of validators
+		s.Require().Eventuallyf(func() bool {
+			newQ := types.NewQueryClient(clientCtx)
+			res, err := newQ.Validator(context.Background(), &types.QueryValidatorRequest{ValidatorAddr: firstValidator})
+			if err != nil {
+				s.T().Logf("error: %s", err)
+				return false
+			}
+			s.T().Logf("validator response: %s", res.GetValidator())
+			return true
+		}, 20*time.Second, 1*time.Second, "can't find slashing info")
 
 		bondTokens := sdk.TokensFromConsensusPower(900000, sdk.DefaultPowerReduction)
 		bondCoin := sdk.NewCoin("testgb", bondTokens)
@@ -50,6 +63,28 @@ func (s *IntegrationTestSuite) TestValidatorOut() {
 			return true
 		}, 5*time.Minute, 10*time.Second, "Delegate to validator failed will retry")
 
+		// Query or send batchtx.
+		s.Require().Eventuallyf(func() bool {
+			queryClient := gravity.BatchTx{
+				BatchNonce: 1,
+				Transactions: []*gravity.SendToEthereum{
+					gravity.NewSendToEthereumTx(2, myTokenContractAddr, mySender, myReceiver, 101, 3),
+					gravity.NewSendToEthereumTx(3, myTokenContractAddr, mySender, myReceiver, 102, 2),
+				},
+				TokenContract: myTokenContractAddr.Hex(),
+				Height:        1234567,
+			}
+
+			mm := gravity.NewMsgRequestBatchTx()
+			res, err := queryClient.BatchTx(context.Background(), &gravity.BatchTxRequest{})
+			if err != nil {
+				s.T().Logf("error: %s", err)
+				return false
+			}
+			s.T().Logf("validator response: %s", res)
+			return true
+		}, 20*time.Second, 1*time.Second, "can't batch tx")
+
 		// Check jail status of validators
 		s.Require().Eventuallyf(func() bool {
 			newQ := types.NewQueryClient(clientCtx)
@@ -58,7 +93,7 @@ func (s *IntegrationTestSuite) TestValidatorOut() {
 				s.T().Logf("error: %s", err)
 				return false
 			}
-			s.T().Logf("validator response: %s", res)
+			s.T().Logf("validator response: %s", res.GetValidator())
 			return true
 		}, 20*time.Second, 1*time.Second, "can't find slashing info")
 
