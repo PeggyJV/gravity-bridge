c9e31ce625b5eb5b8a7cd67877776a9265b604f5
Phil <pbal@fordham.edu>
2022-07-29 16:40:13 -0400

    Cosmos -> Eth test

--- integration_tests/transaction_stress_test.go
+++ integration_tests/transaction_stress_test.go
@@ -14,7 +14,8 @@ import (
 
 // We have 4 validators running so this totals to 100 tx's
 const transactions_per_validator int64 = 25
-const sent_amt int64 = 100
+const cosmos_sent_amt int64 = 100
+const eth_sent_amt int64 = 1
 
 func (s *IntegrationTestSuite) TestTransactionStress() {
 	s.Run("Transaction stress test", func() {
@@ -30,7 +31,7 @@ func (s *IntegrationTestSuite) TestTransactionStress() {
 			s.Require().Equal(sdk.NewUint(10000).BigInt(), balance.BigInt(), "balance was %s, expected 10000", balance.String())	
 		}
 
-		sendAmt := sdk.NewInt(sent_amt)
+		sendAmt := sdk.NewInt(cosmos_sent_amt)
 		// Send many tx's through to cosmos
 		for i := 0; i < len(s.chain.validators); i++ {
 			s.T().Logf("sending %d tx's to cosmos for validator %d ..", transactions_per_validator, i+1)
@@ -75,7 +76,7 @@ func (s *IntegrationTestSuite) TestTransactionStress() {
 				gravityDenom = denomRes.Denom
 
 				for _, coin := range res.Balances {
-					if coin.Denom == gravityDenom && coin.Amount.Equal(sdk.NewInt(sent_amt * transactions_per_validator)) {
+					if coin.Denom == gravityDenom && coin.Amount.Equal(sdk.NewInt(cosmos_sent_amt * transactions_per_validator)) {
 						s.T().Logf("Expected funds recieved for validator %d, balance: %v", i + 1, coin)
 						return true
 					}
@@ -88,7 +89,60 @@ func (s *IntegrationTestSuite) TestTransactionStress() {
 		}
 		fmt.Println("Ethereum -> Cosmos stress test completed.")
 
-	
+		for i := 0; i < len(s.chain.validators); i++ {
+			s.Require().Eventuallyf(func() bool {
+				s.T().Logf("sending %d tx's to ethereum for validator %d ..", transactions_per_validator, i+1)
+
+				validator := s.chain.validators[i]
+				sendToEthereumMsg := types.NewMsgSendToEthereum(
+					validator.keyInfo.GetAddress(),
+					s.chain.validators[len(s.chain.validators)-1-i].ethereumKey.address,
+					sdk.Coin{Denom: gravityDenom, Amount: sdk.NewInt(eth_sent_amt)},
+					sdk.Coin{Denom: gravityDenom, Amount: sdk.NewInt(1)},
+				)
+
+				keyring, err := validator.keyring()
+				s.Require().NoError(err)
+				clientCtx, err := s.chain.clientContext("tcp://localhost:26657", &keyring, "val", validator.keyInfo.GetAddress())
+				s.Require().NoError(err)
+
+				for j := 0; j < int(transactions_per_validator); j++ {
+					response, err := s.chain.sendMsgs(*clientCtx, sendToEthereumMsg)
+					if err != nil {
+						s.T().Logf("error: %s", err)
+						return false
+					}
+					if response.Code != 0 {
+						if response.Code != 32 {
+							s.T().Log(response)
+						}
+						return false
+					}
+				}
+
+				s.T().Logf("%d Tx sent.", transactions_per_validator)
+				return true
+			}, 105*time.Second, 10*time.Second, "unable to send to ethereum")
+		}
+
+		for i := 0; i < len(s.chain.validators); i++ {
+			s.Require().Eventuallyf(func() bool {
+				s.T().Logf("Checking validator %d", i+1)
+
+				balance, err := s.getEthTokenBalanceOf(common.HexToAddress(s.chain.validators[i].ethereumKey.address), testERC20contract)
+				s.Require().NoError(err, "error getting destination balance")
+
+				if balance.LT(sdk.NewInt(10000 - (cosmos_sent_amt * transactions_per_validator) + (eth_sent_amt * transactions_per_validator))) {
+					s.T().Logf("funds not received yet, dest balance: %s", balance.String())
+					return false
+				}
+				
+				s.T().Logf("Funds recieved for validator %d, current balance: %v", i+1, balance.String())
+				return true
+			}, time.Second*180, time.Second*10, "balance never found")
+		}
+		fmt.Println("Cosmos -> Ethereum stress test completed.")
+
 		fmt.Println("StressTestTransaction completed successfully")
 	})
 }
