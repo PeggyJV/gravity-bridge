8066c1fb9bdcc84ee7d62f7b4b3162f3cddf4bda
hannydevelop <ukpaiugochiibem@gmail.com>
2022-08-02 16:14:28 +0100

    WIP, added batch tx and send to ethereum

--- integration_tests/validator_out.go
+++ integration_tests/validator_out.go
@@ -6,6 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/ethereum/go-ethereum/common"
 	gravity "github.com/peggyjv/gravity-bridge/module/v2/x/gravity/types"
 )
 
@@ -16,9 +17,16 @@ func (s *IntegrationTestSuite) TestValidatorOut() {
 	s.Run("Bring up chain, and test the valset update", func() {
 		// remove fourth validator's orchestrator
 		s.dockerPool.RemoveContainerByName("orchestrator3")
-		val := s.chain.validators[1]
 
-		firstValidator := sdk.ValAddress(s.chain.validators[3].keyInfo.GetAddress()).String()
+		validator := s.chain.validators[3]
+
+		valAddr := validator.keyInfo.GetAddress()
+
+		firstValidator := sdk.ValAddress(valAddr).String()
+
+		ethereumAddr := s.chain.validators[3].ethereumKey.address
+
+		var gravityDenom string
 
 		orchKey := s.chain.orchestrators[1]
 		keyring := orchKey.keyring
@@ -38,17 +46,45 @@ func (s *IntegrationTestSuite) TestValidatorOut() {
 			return true
 		}, 20*time.Second, 1*time.Second, "can't find slashing info")
 
-		bondTokens := sdk.TokensFromConsensusPower(900000, sdk.DefaultPowerReduction)
-		bondCoin := sdk.NewCoin("testgb", bondTokens)
-
-		delegator := s.chain.orchestrators[1].keyInfo.GetAddress()
-
-		// Delegate about 5% of the total staking power.
+		// Query ERC20 Denom
 		s.Require().Eventuallyf(func() bool {
-			s.T().Logf("Delegating %v to %v in order to generate a validator set update", bondCoin, delegator)
+			balance, err := s.getEthTokenBalanceOf(common.HexToAddress(validator.ethereumKey.address), testERC20contract)
+			s.Require().NoError(err, "error getting balance")
+			s.T().Logf("balance: %s", balance)
 
-			delegate := types.NewMsgDelegate(delegator, sdk.ValAddress(val.keyInfo.GetAddress()), bondCoin)
-			response, err := s.chain.sendMsgs(*clientCtx, delegate)
+			gbQueryClient := gravity.NewQueryClient(clientCtx)
+			denomRes, err := gbQueryClient.ERC20ToDenom(context.Background(),
+				&gravity.ERC20ToDenomRequest{
+					Erc20: testERC20contract.String(),
+				})
+			if err != nil {
+				s.T().Logf("error querying ERC20 denom %s, %e", testERC20contract.String(), err)
+				return false
+			}
+			s.Require().False(denomRes.CosmosOriginated, "ERC20-originated denom marked as cosmos originated")
+			gravityDenom = denomRes.Denom
+
+			return true
+
+		}, 5*time.Minute, 1*time.Second, "can't query ERC2O Denom")
+
+		// Send Cosmos -> Ethereum transaction
+		s.Require().Eventuallyf(func() bool {
+			s.T().Logf("sending transaction between Cosmos and Ethereum account")
+			Transaction := gravity.NewMsgSendToEthereum(
+				valAddr,
+				ethereumAddr,
+				sdk.Coin{Denom: gravityDenom, Amount: sdk.NewInt(1)},
+				sdk.Coin{Denom: gravityDenom, Amount: sdk.NewInt(1)},
+			)
+
+			keyRing, err := validator.keyring()
+			s.Require().NoError(err)
+
+			clientCtx, err := s.chain.clientContext("tcp://localhost:26657", &keyRing, "val", valAddr)
+			s.Require().NoError(err)
+
+			response, err := s.chain.sendMsgs(*clientCtx, Transaction)
 			if err != nil {
 				s.T().Logf("error: %s", err)
 				return false
@@ -61,29 +97,33 @@ func (s *IntegrationTestSuite) TestValidatorOut() {
 				return false
 			}
 			return true
-		}, 5*time.Minute, 10*time.Second, "Delegate to validator failed will retry")
 
-		// Query or send batchtx.
+		}, 5*time.Minute, 1*time.Second, "can't send funds from Cosmos to Eth account successfully")
+
+		// Create Transaction batch
 		s.Require().Eventuallyf(func() bool {
-			queryClient := gravity.BatchTx{
-				BatchNonce: 1,
-				Transactions: []*gravity.SendToEthereum{
-					gravity.NewSendToEthereumTx(2, myTokenContractAddr, mySender, myReceiver, 101, 3),
-					gravity.NewSendToEthereumTx(3, myTokenContractAddr, mySender, myReceiver, 102, 2),
-				},
-				TokenContract: myTokenContractAddr.Hex(),
-				Height:        1234567,
-			}
+			batchTx := gravity.NewMsgRequestBatchTx(gravityDenom, valAddr)
 
-			mm := gravity.NewMsgRequestBatchTx()
-			res, err := queryClient.BatchTx(context.Background(), &gravity.BatchTxRequest{})
+			keyRing, err := validator.keyring()
+			s.Require().NoError(err)
+
+			clientCtx, err := s.chain.clientContext("tcp://localhost:26657", &keyRing, "val", valAddr)
+			s.Require().NoError(err)
+
+			response, err := s.chain.sendMsgs(*clientCtx, batchTx)
 			if err != nil {
 				s.T().Logf("error: %s", err)
 				return false
 			}
-			s.T().Logf("validator response: %s", res)
+
+			if response.Code != 0 {
+				if response.Code != 32 {
+					s.T().Log(response)
+				}
+				return false
+			}
 			return true
-		}, 20*time.Second, 1*time.Second, "can't batch tx")
+		}, 5*time.Minute, 1*time.Second, "can't create TX batch successfully")
 
 		// Check jail status of validators
 		s.Require().Eventuallyf(func() bool {
