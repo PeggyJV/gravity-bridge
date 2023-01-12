bed26fa800d842a3a268fbc87c3e653ee456f10f
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2023-01-12 17:50:23 +0700

    check and see if test fails in CI as well

--- Makefile
+++ Makefile
@@ -81,3 +81,6 @@ e2e_validator_out: e2e_clean_slate
 
 e2e_transaction_stress: e2e_clean_slate
 	integration_tests/integration_tests.test -test.failfast -test.v -test.run IntegrationTestSuite -testify.m TestTransactionStress || make -s fail
+
+e2e_evm_upgrade: e2e_clean_slate
+	integration_tests/integration_tests.test -test.failfast -test.v -test.run IntegrationTestSuite -testify.m TestMultiEVMUpgrade || make -s fail
---
+++ integration_tests/upgrade_test.go
@@ -0,0 +1,75 @@
+package integration_tests
+
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+)
+
+func (s *IntegrationTestSuite) TestMultiEVMUpgrade() {
+	s.Run("bring up chain, test upgrade path", func() {
+		var err error
+
+		val := s.chain.validators[0]
+		kb, err := val.keyring()
+		s.Require().NoError(err)
+		clientCtx, err := s.chain.clientContext("tcp://localhost:26657", &kb, "val", val.keyInfo.GetAddress())
+		s.Require().NoError(err)
+
+		upgradeQueryClient := upgradetypes.NewQueryClient(clientCtx)
+		govQueryClient := govtypes.NewQueryClient(clientCtx)
+
+		// validate that there is no upgrade planned yet
+		currentPlan, err := upgradeQueryClient.CurrentPlan(context.Background(), &upgradetypes.QueryCurrentPlanRequest{})
+		s.Require().NoError(err)
+		s.Require().Nil(currentPlan.Plan)
+
+		rpcClient, err := rpchttp.New("tcp://localhost:26657", "/websocket")
+		s.Require().NoError(err)
+		status, err := rpcClient.Status(context.Background())
+
+		proposalMsg, err := govtypes.NewMsgSubmitProposal(
+			&upgradetypes.SoftwareUpgradeProposal{
+				Title:       "test multi-evm upgrade",
+				Description: "test multi-evm upgrade description",
+				Plan: upgradetypes.Plan{
+					Name:   "multi-evm-upgrade",
+					Height: status.SyncInfo.LatestBlockHeight + 10,
+				},
+			},
+			sdk.Coins{
+				{
+					Denom:  testDenom,
+					Amount: sdk.NewInt(2),
+				},
+			},
+			val.keyInfo.GetAddress(),
+		)
+
+		s.T().Log("submit proposal upgrading software version")
+		submitProposalResponse, err := s.chain.sendMsgs(*clientCtx, proposalMsg)
+		s.Require().NoError(err)
+		s.Require().Zero(submitProposalResponse.Code, "raw log: %s", submitProposalResponse.RawLog)
+
+		s.T().Log("check proposal was submitted correctly")
+		proposalsQueryResponse, err := govQueryClient.Proposals(context.Background(), &govtypes.QueryProposalsRequest{})
+		s.Require().NoError(err)
+		s.Require().NotEmpty(proposalsQueryResponse.Proposals)
+		s.Require().Equal(govtypes.StatusVotingPeriod, proposalsQueryResponse.Proposals[0].Status, "proposal not in voting period")
+
+		s.T().Log("vote for upgrade proposal")
+		for _, val := range s.chain.validators {
+			kr, err := val.keyring()
+			s.Require().NoError(err)
+			clientCtx, err := s.chain.clientContext("tcp://localhost:26657", &kr, "val", val.keyInfo.GetAddress())
+			s.Require().NoError(err)
+
+			voteMsg := govtypes.NewMsgVote(val.keyInfo.GetAddress(), 0, govtypes.OptionYes)
+			voteResponse, err := s.chain.sendMsgs(*clientCtx, voteMsg)
+			s.Require().NoError(err)
+			s.Require().Zero(voteResponse.Code, "vote error: %s", voteResponse.RawLog)
+		}
+	})
+}
