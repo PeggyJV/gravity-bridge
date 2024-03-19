cfe51e171adeb983677f95eec35ff62cc71f2227
Collin Brittain <collinbrittain@gmail.com>
2024-03-19 13:16:24 -0500

    Update proto build

--- module/contrib/local/protocgen.sh
+++ module/contrib/local/protocgen.sh
@@ -2,24 +2,19 @@
 
 set -eo pipefail
 
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
+ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
 
-  # # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-  # buf protoc \
-  # -I "proto" \
-  # -I "third_party/proto" \
-  # --grpc-gateway_out=logtostderr=true:. \
-  # $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-done
+cd $ROOT/module/proto
+echo "generating proto and gRPC gateway files..."
+buf generate --template buf.gen.gogo.yaml
+cd ..
 
 # move proto files to the right places
-cp -r github.com/peggyjv/gravity-bridge/module/* ./
+xpath=$(head -n 1 go.mod | sed 's/^module //')
+cp -r $xpath/* ./
+
+echo "cleaning up..."
+go mod tidy
 rm -rf github.com
+
+echo "done"
