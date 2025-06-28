f25dca1209fce435e209691cc9598d706d0e26e9
Zaki <zaki@iqlusion.io>
2025-06-28 17:08:16 +0100

    Handle the other confirms and correct v values.

--- orchestrator/gravity/src/query.rs
+++ orchestrator/gravity/src/query.rs
@@ -77,6 +77,7 @@ pub async fn get_all_valset_confirms(
                     corrected_sig[64] = 28;
                     let mut corrected = item.clone();
                     corrected.signature = corrected_sig;
+                    debug!("Corrected the V value on signature");
                     parsed_confirms.push(ValsetConfirmResponse::from_proto(corrected)?);
                     continue;
                     //Do not skip the signature if the V value is invalid.
@@ -160,7 +161,21 @@ pub async fn get_transaction_batch_signatures(
             );
             debug!("reason given for invalid signature: {e:?}");
 
-            continue;
+            match e {
+                clarity::Error::InvalidV => {
+                    let mut corrected_sig = confirm.signature.clone();
+                    corrected_sig[64] = 28;
+                    let mut corrected = confirm.clone();
+                    corrected.signature = corrected_sig;
+                    debug!("Corrected the V value on signature");
+                    out.push(BatchConfirmResponse::from_proto(corrected)?);
+                    continue;
+                    //Do not skip the signature if the V value is invalid.
+                }
+                _ => {
+                    continue;
+                }
+            }
         }
 
         out.push(BatchConfirmResponse::from_proto(confirm)?)
@@ -223,9 +238,21 @@ pub async fn get_logic_call_signatures(
                 "ignoring logic call confirmation with invalid signature from {}",
                 confirm.ethereum_signer
             );
-            debug!("reason given for invalid signature: {e:?}");
-
-            continue;
+            match e {
+                clarity::Error::InvalidV => {
+                    let mut corrected_sig = confirm.signature.clone();
+                    corrected_sig[64] = 28;
+                    let mut corrected = confirm.clone();
+                    corrected.signature = corrected_sig;
+                    debug!("Corrected the V value on signature");
+                    out.push(LogicCallConfirmResponse::from_proto(corrected)?);
+                    continue;
+                    //Do not skip the signature if the V value is invalid.
+                }
+                _ => {
+                    continue;
+                }
+            }
         }
 
         out.push(LogicCallConfirmResponse::from_proto(confirm)?)
